49319ce73a6e55bc3c518791901b0c5a1913b245
horst <yanglongcui@gmail.com>
2016-12-09 23:28:59 -0500

    add localhost test for socket

---
+++ socket/server-localhost.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = ''
+PORT = 1989
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(1)
+conn,addr = s.accept()
+while 1:
+	conn.send('Hi this is server')
+	data = conn.recv(1024)
+	if not data : break
+	if data == 'Yes' :
+		print "He said yes"
+	print addr
+	print 'I got your message = ' + data
+conn.close()
+s.close()
+
