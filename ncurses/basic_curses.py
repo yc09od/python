48ef9982b13cebfa5eb554a9dc9712033de2e76d
horst <yanglongcui@gmail.com>
2017-01-06 00:49:06 -0500

    Add output board to room, Add name function to room,Add commend exit to room

--- ncurses/basic_curses.py
+++ ncurses/basic_curses.py
@@ -1,3 +1,4 @@
+# -*- coding: ascii -*-
 import curses
 import traceback
 import time
@@ -9,6 +10,7 @@ def main(stdscr) :
 
 	screen = stdscr.subwin(23,79,0,0)
 	inputScreen = stdscr.subwin(1,77,21,1)
+        outputScreen = stdscr.subwin(19,77,1,1)
 	subScreen = curses.newwin(10,10,0,79)
 	screen.box()
 	subScreen.box()
@@ -17,16 +19,37 @@ def main(stdscr) :
 	screen.refresh()
 	subScreen.refresh()
 	input_bar = []
+        output_data = []
+        name = ''
 	while True :
+            try:
 		ch = screen.getch()
-		if (ch == 8) and len(input_bar) > 0 :
-			input_bar.pop()	
+                #if ch is delete or backspace
+		if (ch == 127 or ch == 8) and len(input_bar) > 0 :
+		    input_bar.pop()	
+                elif (ch == 127 or ch == 8) and len(input_bar) <=0 :
+                    pass
+                elif ch == 10 :
+                    outputScreen.erase()
+                    if ''.join(input_bar) == ':exit' :
+                        break
+                    elif ':name' in ''.join(input_bar) :
+                        name = ''.join(input_bar)[len(':name') + 1 :] 
+                    else :
+                        output_data.append((name + ' : ' if len(name) > 0 else '') + ''.join(input_bar))
+                    for i in range(0 if len(output_data) <= 19 else len(output_data) - 19 ,len(output_data)) :
+                            outputScreen.addstr(i if len(output_data) <= 19 else i - len(output_data) + 19,0,output_data[i])
+                    outputScreen.refresh()
+                    input_bar = []
 		else :
-			input_bar.append(chr(ch))
+		    input_bar.append(chr(ch))
 		inputScreen.erase()
 		inputScreen.addstr(0,0,''.join(input_bar))
 		inputScreen.refresh()
-	
+            except KeyboardInterrupt:
+                break
+            except :
+                pass
 try :
 	
     if __name__ == "__main__" :
